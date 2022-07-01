6411ebaefeed7901ecdd489ec788c3d0a03489b1
Juan Espitia <juanespitia@outlook.com>
2022-07-01 05:33:19 -0500

    validations_query

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 #FastAPI
 from fastapi import FastAPI
 from fastapi import Body
+from fastapi import Query
 
 app = FastAPI()
 
@@ -26,3 +27,24 @@ def home():
 @app.post('/person/new')
 def create_person(person: Person = Body(...)):
   return person
+
+#validations: query parameters
+#validate strings
+# max_length
+# min_length
+# regex
+#validate int
+# ge -> great or equal than >=
+# le -> less or equal than <=
+# gt -> greater than >
+# lt -> less than <
+#documentation in query params
+#title
+#description
+
+@app.get('/person/detail')
+def show_person(
+  name: Optional[str] = Query(None, min_length=1, max_length=50),
+  age: Optional[str] = Query(...)
+):
+  return {name: age}
